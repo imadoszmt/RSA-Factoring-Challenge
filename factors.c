82a85d6b9091dcaaf04a0eea8c6e875c0a6a523f
Imad Zalmat <imaden.zalmat@gmail.com>
2024-04-07 09:59:25 -0700

    Change order of printing

--- factors.c
+++ factors.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[]) {
 	{
             if (num % factor == 0) 
 	    {
-                printf("%d=%d*%d\n", num, factor, num / factor);
+                printf("%d=%d*%d\n", num, num / factor,factors.cfactors.cfactor);
                 break;
             }
             factor++;
