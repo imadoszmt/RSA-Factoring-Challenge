a51ba32cd4a7c293f51f1657007f85fbdddae2af
Imad Zalmat <imaden.zalmat@gmail.com>
2024-04-07 19:48:13 -0700

    Return to recover copy of the code

--- factors.c
+++ factors.c
@@ -1,35 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void factorize(int value) {
-    int i = 2;
-    
-    if (value < 2)
-        return;
-    
-    while (value % i)
-        i++;
-    
-    printf("%.0f=%.0f*%.0f\n", (float)value, (float)value / i, (float)i);
-}
-
 int main(int argc, char *argv[]) {
-    if (argc != 2)
-        exit(EXIT_FAILURE);
-    
-    FILE *file = fopen(argv[1], "r");
-    if (file == NULL) {
-        perror("Error opening file");
-        exit(EXIT_FAILURE);
+    if (argc != 2) 
+    {
+        return EXIT_FAILURE;
     }
-    
-    char line[100];
-    while (fgets(line, sizeof(line), file) != NULL) {
-        int value = atoi(line);
-        factorize(value);
-    }
-    
-    fclose(file);
-    return 0;
-}
 
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL) 
+    {
+        perror("fopen");
+        return EXIT_FAILURE;
+    }
+
+    int num;
+
+    while (fscanf(fp, "%d", &num) == 1) 
+    {
+        if (num <= 1) 
+	{
+            continue;
+        }
+
+        int factor = 2;
+        while (factor * factor <= num) 
+	{
+            if (num % factor == 0) 
+	    {
+                printf("%d=%d*%d\n", num, num / factor, factor);
+                break;
+            }
+            factor++;
+        }
+
+    }
+
+    fclose(fp);
+    return EXIT_SUCCESS;
+}
